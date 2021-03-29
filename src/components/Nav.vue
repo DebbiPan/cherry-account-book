79d3c8fb0ce05e947b68218bef19fc101add51be
Debbie <1770554944@qq.com>
2021-03-29 11:54:50 +0800

    全局组件<Nav/>

---
+++ src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+<div>我是全局组件</div>
+</template>
+
+<script>
+export default {
+name: "Nav"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Labels.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    labels
+    <Nav/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "labels"
+}
+</script>
+
+<style scoped>
+
+</style>
