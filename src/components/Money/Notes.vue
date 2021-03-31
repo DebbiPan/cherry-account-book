1dd2d5a854272feb09e3c41147115d4ea71a9045
Debbie <1770554944@qq.com>
2021-03-31 13:07:05 +0800

    使用v-model绑定value

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,34 +1,41 @@
 <template>
   <div>
+    {{value}}
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="在这里输入备注">
+      <input v-model="value" type="text" placeholder="在这里输入备注">
     </label>
   </div>
 </template>
 
-<script>
-export default {
-name: "Notes"
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Notes extends Vue {
+  value = '';
 }
 </script>
 
 <style lang="scss" scoped>
-.notes{
-  font-size:14px;
-  background:#F8F6FF;
+.notes {
+  font-size: 14px;
+  background: #F8F6FF;
   padding-left: 16px;
-  display:flex;
-  align-items:center;
-  .name{
-    padding-right:16px;
+  display: flex;
+  align-items: center;
+
+  .name {
+    padding-right: 16px;
   }
-  input{
-    background:transparent;
-    height:64px;
+
+  input {
+    background: transparent;
+    height: 64px;
     flex-grow: 1;
     border: none;
-    padding-right:16px;
+    padding-right: 16px;
   }
 }
 </style>
