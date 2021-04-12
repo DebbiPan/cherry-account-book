612c03b38a65d0a58272215666990d55faae7f97
Debbie <1770554944@qq.com>
2021-04-12 15:34:50 +0800

    完成统计页面的css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,18 +3,15 @@
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" height="48px"/>
     <div>
-      type:{{ type }}
-      <br>
-      interval:{{ interval }}
-    </div>
-    <div>
       <div>
         <ol>
-          <li v-for="(group,index) in result" :key="index.id">
-            <h3>{{group.title}}</h3>
+          <li v-for="(group,index) in result" :key="index">
+            <h3 class="title">{{ group.title }}</h3>
             <ol>
-              <li v-for="item in group.items" :key="item.id">
-                {{ item.amount }}
+              <li v-for="item in group.items" :key="item.id" class="record">
+                <span>{{tagString(item.tags)}}</span>
+                <span class="notes">{{item.notes}}</span>
+                <span>¥{{ item.amount }}</span>
               </li>
             </ol>
           </li>
@@ -35,6 +32,10 @@ import {Component} from 'vue-property-decorator';
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
@@ -49,7 +50,6 @@ export default class Statistics extends Vue {
       hashTable[date] = hashTable[date] || {title: date, items: []};
       hashTable[date].items.push(recordList[i]);
     }
-    console.log(hashTable);
     return hashTable;
   }
 
@@ -79,10 +79,29 @@ export default class Statistics extends Vue {
       }
     }
   }
+}
 
-  .interval-tabs-item {
-    //height: 48px;
-  }
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  background: #fff;
+  @extend %item;
+}
+
+.notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
 }
 
 </style>
